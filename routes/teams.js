1198d43803f8ba15f2316783c7a755a734d25424
Roland Saavedra <rolandsaavedra22@gmail.com>
2022-02-23 02:21:57 -0500

    bomb has been planted

--- routes/teams.js
+++ routes/teams.js
@@ -5,12 +5,10 @@ const createIdeas = require("../test-funcs")
 const { FieldValue } = require('firebase-admin/firestore');
 
 router.get('/', (req, res) =>{
-    console.log("visited teams")
     const ideas = [];
     const idea = db.collection('ideas').get()
     .then((snapshot) => {
         snapshot.forEach(idea => {
-            console.log(idea.id)
             ideas.push({
                 name: idea.data().name,
                 idea: idea.data().idea,
