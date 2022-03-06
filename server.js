e0fa683220d88e304856fa72c87d7c841222ecee
Roland Saavedra <rolandsaavedra22@gmail.com>
2022-03-06 00:11:56 -0500

    works better now

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ app.use(express.static('public'))
 app.use('/teams', teamRouter)
 
 app.get('/', (req, res) => {
-    res.redirect('/contamihax.html')
+    res.redirect('/ourchapter.html')
     console.log("Went home")
 })
 
 app.get('/chapter', (req, res) => {
     res.redirect('/ourchapter.html')
+})
+
+app.get('/contamihax', (req, res) => {
+    res.redirect('/contamihax.html')
 })
 
 
